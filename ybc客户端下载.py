d2edbffea39cc73a419cf52ec81162b1e57d5498
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2021-11-26 17:38:33 +0800

    Update the webside

--- ybc客户端下载.py
+++ ybc客户端下载.py
@@ -5,5 +5,5 @@ import urllib.request as ur
 import os
 
 
-ur.urlretrieve('https://planet.fbcontent.cn/apps/download/yuanbiancheng-3.1.1.exe','ybc客户端安装程序.exe')
+ur.urlretrieve('https://planet.fbcontent.cn/apps/download/yuanbiancheng-3.3.0.exe','ybc客户端安装程序.exe')
 os.system('ybc客户端安装程序.exe')
